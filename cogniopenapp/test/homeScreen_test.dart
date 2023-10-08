f0de43d616f108be12da2dc8d389743a3012631e
vivekgoel1000 <vivekgoel1000@gmail.com>
2023-10-08 17:32:12 -0400

    Simulating tap
    Trying to tap the 'Gallery' button programmatically.

--- cogniopenapp/test/homeScreen_test.dart
+++ cogniopenapp/test/homeScreen_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cogniopenapp/ui/galleryScreen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cogniopenapp/main.dart';
 
@@ -12,14 +14,35 @@ void main() {
   testWidgets(
       'Tests that the title and subtitle of the application displays after starting.',
       (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and trigger a frrame.
     await tester.pumpWidget(MyApp());
+    await tester.pumpAndSettle();
 
     // Verify that our counter starts at 0.
-    expect(find.text('CogniOpen'), findsOneWidget);
-    expect(
+    expect(find.text('CogniOpen', skipOffstage: false), findsOneWidget);
+    debugPrint("-------");
+    final galleryButtonFinder2 =
+        find.byKey(const Key("GalleryButtonKey"), skipOffstage: false);
+    debugPrint("*******");
+    debugPrint(galleryButtonFinder2.toString());
+    debugPrint("*******");
+    await tester.tap(galleryButtonFinder2);
+    final iconButtonFinder = find.byType(IconButton, skipOffstage: false);
+    debugPrint(iconButtonFinder.toString());
+    debugPrint("________");
+
+    //final elevatedButtonFinder =
+    //  find.byType(ElevatedButton, skipOffstage: false);
+    // debugPrint(elevatedButtonFinder.toString());
+    //final galleryScreenFinder = find.byType(GalleryScreen, skipOffstage: false);
+    // expect(galleryScreenFinder, findsOneWidget);
+    debugPrint("*******");
+    // debugPrint(galleryScreenFinder.toString());
+    debugPrint("*******");
+
+/*    expect(
         find.text(
             'Helping you remember the important thing\n Choose a feature from here to get started!'),
-        findsOneWidget);
+        findsOneWidget);*/
   });
 }
