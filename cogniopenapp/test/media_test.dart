79e364179708987a9bab01e29549411eb63a2fd6
LAURA HAMANN <Laura Hamann@rdhflad.faa>
2023-10-09 15:56:59 -0400

    adding in unit tests for media, settings and video classes

--- cogniopenapp/test/media_test.dart
+++ cogniopenapp/test/media_test.dart
@@ -28,33 +28,48 @@ void main() {
     expect(media.tags![1], "spunky");
   });
 
-  test('testing getStorageSizeString', () {
+  test('testing getStorageSizeString KB', () {
     //KB
     expect(testMedia[0].getStorageSizeString(), '1.00 KB');
+  });
+
+  test('testing getStorageSizeString Bytes', () {
     //Bytes
     expect(testMedia[2].getStorageSizeString(), '512 Bytes');
+  });
+
+  test('testing getStorageSizeString MB', () {
     //MB
     testMedia[3].storageSize = 10000000;
     expect(testMedia[3].getStorageSizeString(), '9.54 MB');
+  });
+
+  test('testing getStorageSizeString GB', () {
     //GB
     testMedia[3].storageSize = 10000000000;
     expect(testMedia[3].getStorageSizeString(), '9.31 GB');
   });
 
-  test('testing getDateTimeString', () {
+  test('testing getDateTimeString valid DateTime', () {
     //regular datetime
     expect(testMedia[6].getDateTimeString(), '2023-10-05 00:00:00');
-
-    //null
-    testMedia[6].timeStamp = null;
-    expect(testMedia[6].getDateTimeString(), 'Date unknown');
   });
 
-  test('testing formatDateTime', () {
+  test('testing getDateTimeString null DateTime', () {
+    //null
+    DateTime? originalDateTime = testMedia[6].timeStamp;
+    testMedia[6].timeStamp = null;
+    expect(testMedia[6].getDateTimeString(), 'Date unknown');
+    testMedia[6].timeStamp = originalDateTime;
+  });
+
+  test('testing formatDateTime, valid Date', () {
     //regular datetime
     expect(testMedia[6].formatDateTime(testMedia[6].timeStamp),
         'October 5th, 2023');
+  });
 
+  test('testing formatDateTime, null date', () {
     //null
     expect(testMedia[6].formatDateTime(null), 'N/A');
   });
