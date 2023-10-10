a39bf65bf2a0c8522e89143104ecbabc97206b86
abebe-umgc <anatea@student.umgc.edu>
2023-10-10 08:20:50 -0400

    resolving the import package conflict issue

--- cogniopenapp/lib/main.dart
+++ cogniopenapp/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'ui/homescreen.dart';
+import 'package:cogniopenapp/ui/homeScreen.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
