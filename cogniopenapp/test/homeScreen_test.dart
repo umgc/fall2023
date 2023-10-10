6097a19c9672b2632c7231c07a4080c3c50c4e5c
vivekgoel1000 <vivekgoel1000@gmail.com>
2023-10-09 21:42:18 -0400

    Testing all buttons
    Used ensureVisible to test remaining buttons

--- cogniopenapp/test/homeScreen_test.dart
+++ cogniopenapp/test/homeScreen_test.dart
@@ -1,9 +1,4 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Tests CogniOpen home screen
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -18,18 +13,6 @@ void main() {
 
     // Verify that our counter starts at 0.
     expect(find.text('CogniOpen', skipOffstage: false), findsOneWidget);
-<<<<<<< Updated upstream
-    expect(find.byKey(const Key("GalleryButtonKey"), skipOffstage: false), findsOneWidget);
-    expect(find.byKey(const Key("VirtualAssistantButtonKey"), skipOffstage: false), findsOneWidget);
-    expect(find.byKey(const Key("AudioRecordingButtonKey"), skipOffstage: false), findsOneWidget);
-
-    // These following two tests are failing.  But the buttons are there when typing "flutter run".
-    // It seems like not all the buttons show up with "flutter test" for some reason.  Commenting out
-    // for now.
-    //
-    //expect(find.byKey(const Key("SearchButtonKey"), skipOffstage: false), findsOneWidget);
-    //expect(find.byKey(const Key("RecentRequestsButtonKey"), skipOffstage: false), findsOneWidget);
-=======
     expect(find.widgetWithText(ElevatedButton, "Virtual Assistant", skipOffstage: false), findsOneWidget);
     expect(find.widgetWithText(ElevatedButton, "Gallery", skipOffstage: false), findsOneWidget);
     expect(find.widgetWithText(ElevatedButton, "Video Recording", skipOffstage: false), findsOneWidget);
@@ -39,6 +22,5 @@ void main() {
     await tester.pumpAndSettle();
     expect(find.widgetWithText(ElevatedButton, "Search", skipOffstage: false), findsOneWidget);
     expect(find.widgetWithText(ElevatedButton, "Recent Requests", skipOffstage: false), findsOneWidget);
->>>>>>> Stashed changes
   });
 }
