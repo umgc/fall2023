95536373a4d43bd8197e063b40f6f2823f84811c
abebe-umgc <anatea@student.umgc.edu>
2023-10-09 21:48:58 -0400

    adding dotenv package and modified the main file

--- cogniopenapp/lib/main.dart
+++ cogniopenapp/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'ui/homescreen.dart';
 
-
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -12,9 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute:
+          '/biometric_auth', // Set the initial route to biometric authentication
       home: HomeScreen(),
+      // routes: {
+      //   //'/biometric_auth': (context) => BiometricAuthScreen(),
+      //   '/home': (context) => HomeScreen(),
+      //   // Add other routes as needed
+      // },
     );
   }
 }
-
-
