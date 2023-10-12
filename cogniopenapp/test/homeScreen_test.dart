7b685970d822646825302b4d438bacd6f28fecf7
vivekgoel1000 <vivekgoel1000@gmail.com>
2023-10-12 19:28:22 -0400

    Checking buttons on home screen
    The new user interface has extra functionality

--- cogniopenapp/test/homeScreen_test.dart
+++ cogniopenapp/test/homeScreen_test.dart
@@ -8,19 +8,17 @@ void main() {
   testWidgets('Tests that the application home page loads correctly.', (WidgetTester tester) async {
     // Build our app and trigger a frrame.
     await tester.pumpWidget(MyApp());
-    await tester.pumpAndSettle();
-    await tester.pump();
 
-    // Verify that our counter starts at 0.
+    // Check that the title is displayed
     expect(find.text('CogniOpen', skipOffstage: false), findsOneWidget);
+
+    // Check all the buttons are visible
+    expect(find.widgetWithText(ElevatedButton, "Record", skipOffstage: false), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Conversation History", skipOffstage: false), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Significant Objects", skipOffstage: false), findsOneWidget);
     expect(find.widgetWithText(ElevatedButton, "Virtual Assistant", skipOffstage: false), findsOneWidget);
-    expect(find.widgetWithText(ElevatedButton, "Gallery", skipOffstage: false), findsOneWidget);
-    expect(find.widgetWithText(ElevatedButton, "Video Recording", skipOffstage: false), findsOneWidget);
-    final audioRecordingButtonFinder = find.widgetWithText(ElevatedButton, "Audio Recording", skipOffstage: false);
-    expect(audioRecordingButtonFinder, findsOneWidget);
-    await tester.ensureVisible(audioRecordingButtonFinder);
-    await tester.pumpAndSettle();
-    expect(find.widgetWithText(ElevatedButton, "Search", skipOffstage: false), findsOneWidget);
-    expect(find.widgetWithText(ElevatedButton, "Recent Requests", skipOffstage: false), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Tour Guide", skipOffstage: false), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "My Gallery", skipOffstage: false), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Settings", skipOffstage: false), findsOneWidget);
   });
 }
