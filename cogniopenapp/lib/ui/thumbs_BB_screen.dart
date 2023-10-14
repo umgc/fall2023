39caef127b8d6f68fcac9ac509ecb29c3ab36f6f
nother_dave <dbright9@student.umgc.edu>
2023-10-13 21:36:29 -0400

    boundingbox attempt is better

--- cogniopenapp/lib/ui/thumbs_BB_screen.dart
+++ cogniopenapp/lib/ui/thumbs_BB_screen.dart
@@ -8,29 +8,12 @@ class CameraAppScreen extends StatefulWidget {
 
 class CameraAppState extends State<CameraAppScreen> {
   // Holds the position information of the rectangle
-  Map<String, double> _position = {
-    'x': 1,
-    'y': 1,
-    'w': 1,
-    'h': 1,
+  final Map<String, double> _position = {
+    'x': 50,
+    'y': 115,
+    'w': 300,
+    'h': 110,
   };
-
-  // Whether or not the rectangle is displayed
-  bool _isRectangleVisible = true;
-
-  // Some logic to get the rectangle values
-  void updateRectanglePosition() {
-    setState(() {
-      // assign new position
-      _position = {
-        'x': 1,
-        'y': 1,
-        'w': 1,
-        'h': 1,
-      };
-      _isRectangleVisible = true;
-    });
-  }
 
   @override
   void initState() {
@@ -49,39 +32,38 @@ class CameraAppState extends State<CameraAppScreen> {
     return Stack(
       children: [
         const Image(image: AssetImage('assets/test_images/eyeglass-frame.jpg')),
-        if (_isRectangleVisible)
-          Positioned(
-            left: _position['x'],
-            top: _position['y'],
-            child: InkWell(
-              onTap: () {
-                // When the user taps on the rectangle, it will disappear
-                setState(() {
-                  _isRectangleVisible = false;
-                });
-              },
-              child: Container(
-                width: _position['w'],
-                height: _position['h'],
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2,
-                    color: Colors.yellow,
+        //if (_isRectangleVisible)
+        Positioned(
+          left: _position['x'],
+          top: _position['y'],
+          child: Opacity(
+            opacity: 0.35,
+            child: Material(
+              child: InkWell(
+                child: Container(
+                  width: _position['w'],
+                  height: _position['h'],
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    color: Colors.yellow,
-                    child: const Text(
-                      'horse -71%',
-                      style: TextStyle(color: Colors.white),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      color: Colors.black,
+                      child: const Text(
+                        'glasses -71%',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
           ),
+        ),
       ],
     );
   }
