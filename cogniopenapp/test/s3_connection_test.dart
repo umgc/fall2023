201433ad72edb2bfff12fcb99385c0a3e29688d1
LAURA HAMANN <laura.m.hamann@faa.gov>
2023-10-21 06:20:37 -0400

    initial stubs for s3 tests, including mocks

--- cogniopenapp/test/s3_connection_test.dart
+++ cogniopenapp/test/s3_connection_test.dart
@@ -1,7 +1,33 @@
+//import 'dart:js_util';
+
+//import 'package:aws_s3_api/s3-2006-03-01.dart';
+import 'package:aws_s3_api/s3-2006-03-01.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cogniopenapp/src/s3_connection.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'mocks/s3_connection_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<S3Bucket>()])
+@GenerateNiceMocks([MockSpec<S3>()])
 void main() {
+  final s3Bucket = MockS3Bucket();
+
+  test('s3 bucket created', () async {
+    s3Bucket.createBucket;
+    expect(s3Bucket.connection, null);
+    expect(s3Bucket.toString(), "MockS3Bucket");
+
+    when(s3Bucket.addAudioToS3('testAudio', '\some\localPath'))
+        .thenAnswer((_) => Future.value(''));
+
+    when(s3Bucket.addAudioToS3(
+            'testAudio', '\assets\test_images\Sea waves.mp4'))
+        .thenAnswer((_) => Future.value('testAudio'));
+
+    verifyNever(s3Bucket.addAudioToS3('testAudio', '\some\localPath'));
+  });
 /*
   // Try to create the Amazon S3 Bucket if not already there
   S3Bucket? CogniOpenS3Bucket;
