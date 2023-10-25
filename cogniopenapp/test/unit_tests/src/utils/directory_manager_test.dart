70a5f550a22cd3ab7b9a87c64f6a48b84847726c
Zach Bowman <zach.bowman@yahooinc.com>
2023-10-25 02:25:29 -0400

    Add FakePathProviderPlatform test resource (+path_provider_foundation dependency).

--- cogniopenapp/test/unit_tests/src/utils/directory_manager_test.dart
+++ cogniopenapp/test/unit_tests/src/utils/directory_manager_test.dart
@@ -1,51 +1,25 @@
 import 'dart:io';
-import 'dart:ui';
 
-import 'package:cogniopenapp/main.dart';
 import 'package:cogniopenapp/src/utils/directory_manager.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path_provider_android/path_provider_android.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class FakePathProviderPlatform extends Fake
-    with MockPlatformInterfaceMixin
-    implements PathProviderPlatform {
-  @override
-  Future<String?> getApplicationDocumentsPath() async {
-    return 'testApplicationDocumentsPath';
-  }
-}
+import '../../../resources/fake_path_provider_platform.dart';
 
 void main() {
-  //if (Platform.isAndroid) PathProviderAndroid.registerWith();
-
-  //DartPluginRegistrant.ensureInitialized();
-  TestWidgetsFlutterBinding.ensureInitialized();
-  setUp(() async {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
     PathProviderPlatform.instance = FakePathProviderPlatform();
+    DirectoryManager.instance.initializeDirectories();
   });
 
-  //final Directory documentDirectory = getApplicationDocumentsDirectory();
-  //print(documentDirectory);
-  //initialize DirectoryManager
-  DirectoryManager.instance.initializeDirectories();
-
-  test('verify photo directory was created', () {
-    //expect(DirectoryManager.instance.photosDirectory, isNot(null));
+  test('verify all directores were created', () {
+    expect(DirectoryManager.instance.rootDirectory, isNot(null));
+    expect(DirectoryManager.instance.photosDirectory, isNot(null));
+    expect(DirectoryManager.instance.videosDirectory, isNot(null));
+    expect(DirectoryManager.instance.audiosDirectory, isNot(null));
+    expect(DirectoryManager.instance.videoStillsDirectory, isNot(null));
+    expect(DirectoryManager.instance.videoThumbnailsDirectory, isNot(null));
+    expect(DirectoryManager.instance.tmpDirectory, isNot(null));
   });
-
-  Future<bool> doesDirectoryExist(String directoryPath) async {
-    if (await Directory(directoryPath).exists()) {
-      return Future<bool>.value(true);
-    } else {
-      return Future<bool>.value(false);
-    }
-  }
-
-  Directory getApplicationDocumentsDirectory() {
-    return Directory.current;
-  }
 }
