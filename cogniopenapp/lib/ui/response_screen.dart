02b6a42a44c73679985766f705ddf876997a56bf
Ben Sutter <bsutter@student.umgc.edu>
2023-11-02 15:17:17 -0400

    Extracted common elements to their own widget

--- cogniopenapp/lib/ui/response_screen.dart
+++ cogniopenapp/lib/ui/response_screen.dart
@@ -91,21 +91,20 @@ class _ResponseScreenState extends State<ResponseScreen>
                     ),
                     for (var response in displayedResponses)
                       GestureDetector(
-                        onTap: () {
-                          // Navigate to the VideoResponseGridScreen when a returnTextBox is tapped.
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ImageNavigatorScreen(
-                                  ResponseParser.getRequestedResponseList(
-                                      response.title,
-                                      filterInterval: 3000)),
-                            ),
-                          );
-                        },
-                        child: returnResponseBox(response,
-                            "${response.title}: ${ResponseParser.getTimeStampFromResponse(response)} (${ResponseParser.getHoursFromResponse(response)}) \nADDRRESS"),
-                      ),
+                          onTap: () {
+                            // Navigate to the VideoResponseGridScreen when a returnTextBox is tapped.
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ImageNavigatorScreen(
+                                    ResponseParser.getRequestedResponseList(
+                                        response.title,
+                                        filterInterval: 3000)),
+                              ),
+                            );
+                          },
+                          child: ResponseBox(response,
+                              "${response.title}: ${ResponseParser.getTimeStampFromResponse(response)} (${ResponseParser.getHoursFromResponse(response)}) \nADDRRESS")),
                   ],
                 ),
               ),
@@ -180,21 +179,8 @@ class _ImageNavigatorScreenState extends State<ImageNavigatorScreen>
                   SizedBox(
                     height: 60,
                   ),
-                  returnResponseBox(videoResponse,
+                  ResponseBox(videoResponse,
                       "${ResponseParser.getTimeStampFromResponse(videoResponse)} (${ResponseParser.getHoursFromResponse(videoResponse)}) \nADDRRESS"),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            showConfirmationDialog(videoResponse.title);
-                          },
-                          child: Text("This is the object I was looking for"),
-                        ),
-                      ),
-                    ],
-                  ),
                 ],
               ),
             );
@@ -203,8 +189,93 @@ class _ImageNavigatorScreenState extends State<ImageNavigatorScreen>
       ),
     );
   }
+}
 
-  void showConfirmationDialog(String title) {
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| RESPONSE WIDGET |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||(widget and item creation)||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+class ResponseBox extends StatelessWidget {
+  final VideoResponse response;
+  final String title;
+
+  ResponseBox(this.response, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Stack(
+            children: [
+              FutureBuilder<Image>(
+                future: ResponseParser.getThumbnail(response),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    Image? image = snapshot.data;
+                    double imageHeight = image?.height ?? 0.0;
+                    double imageWidth = image?.width ?? 0.0;
+
+                    return Container(
+                      child: image,
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              getBoundingBox(response),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    showConfirmationDialog(context);
+                  },
+                  child: Text("This is the object I was looking for"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> deletePreviousResponses(String responsesToDelete) async {
+    List<VideoResponse> responses =
+        ResponseParser.getRequestedResponseList(responsesToDelete);
+    print("RESPONSE LENGTH: ${responses.length}");
+    for (VideoResponse response in responses) {
+      print("REMOVED ID: ${response.id!}");
+      await DataService.instance.removeVideoResponse(response.id!);
+    }
+  }
+
+  void showConfirmationDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (context) {
@@ -219,17 +290,20 @@ class _ImageNavigatorScreenState extends State<ImageNavigatorScreen>
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); // Pop one screen
-                    Navigator.of(context).pop(); // Pop the second screen
+                    Navigator.of(context).pop(); // Close the dialog
+                    Navigator.of(context).pop(); // Close the dialog
                   },
                   child: Text("No, keep them"),
                 ),
                 ElevatedButton(
                   onPressed: () async {
-                    await deletePreviousResponses(title);
+                    await deletePreviousResponses(response.title);
+                    Navigator.of(context).pop(); // Close the dialog
+
+                    // Navigate back to the ResponseScreen
                     Navigator.of(context).pop(); // Pop one screen
                     Navigator.of(context).pop(); // Pop the second screen
-                    Navigator.of(context).pop(); // Pop the second screen
+                    Navigator.of(context).pop(); // Pop the third screen
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => ResponseScreen()),
@@ -248,120 +322,40 @@ class _ImageNavigatorScreenState extends State<ImageNavigatorScreen>
     );
   }
 
-  Future<void> deletePreviousResponses(String responsesToDelete) async {
-    List<VideoResponse> responses =
-        ResponseParser.getRequestedResponseList(responsesToDelete);
-    print("RESPONSE LENGTH: ${responses.length}");
-    for (VideoResponse response in responses) {
-      print("REMOVED ID: ${response.id!}");
-      await DataService.instance.removeVideoResponse(response.id!);
-    }
-  }
-}
+  Positioned getBoundingBox(VideoResponse response) {
+    double imageWidth = 400;
+    double imageHeight = 700;
 
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| COMMON FUNCTIONALITY |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||(widget and item creation)||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
-FutureBuilder<Image> getFutureThumbnail(VideoResponse videoResponse) {
-  return FutureBuilder<Image>(
-    future: ResponseParser.getThumbnail(videoResponse),
-    builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.done) {
-        Image? image = snapshot.data; // Get the Image widget from the snapshot
-
-        // Access the height and width of the Image widget with null-aware operators
-        double imageHeight = image?.height ?? 0.0;
-        double imageWidth = image?.width ?? 0.0;
-
-        return Column(
-          children: [
-            Container(
-              child: image, // Wrap Image in a Container
-            ),
-            //Text(
-            //'Height: $imageHeight, Width: $imageWidth'), // Display height and width
-          ],
-        );
-      } else {
-        // While loading the image, you can display a loading indicator or placeholder.
-        return CircularProgressIndicator();
-      }
-    },
-  );
-}
-
-Container returnResponseBox(VideoResponse response, String title) {
-  return Container(
-    padding: const EdgeInsets.all(10.0),
-    width: double.infinity,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10.0),
-      border: Border.all(
-        color: Colors.black,
-        width: 2.0,
-      ),
-    ),
-    child: Column(
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(
-          height: 8,
-        ),
-        Stack(
-          children: [
-            getFutureThumbnail(response),
-            getBoundingBox(response),
-          ],
-        )
-      ],
-    ),
-  );
-}
-
-Positioned getBoundingBox(
-  VideoResponse response,
-  /* Image stillImage*/
-) {
-  double imageWidth = 400;
-  double imageHeight = 700;
-
-  return Positioned(
-    left: imageWidth * response.left,
-    top: imageHeight * response.top,
-    child: Opacity(
-      opacity: 0.35,
-      child: Material(
-        child: InkWell(
-          child: Container(
-            width: imageWidth * response.width,
-            height: imageHeight * response.height,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2,
-                color: Colors.black,
+    return Positioned(
+      left: imageWidth * response.left,
+      top: imageHeight * response.top,
+      child: Opacity(
+        opacity: 0.35,
+        child: Material(
+          child: InkWell(
+            child: Container(
+              width: imageWidth * response.width,
+              height: imageHeight * response.height,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: Colors.black,
+                ),
               ),
-            ),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                color: Colors.black,
-                child: Text(
-                  '${response.title} ${((response.confidence * 100).truncateToDouble()) / 100}%',
-                  style: const TextStyle(color: Colors.white),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  color: Colors.black,
+                  child: Text(
+                    '${response.title} ${((response.confidence * 100).truncateToDouble()) / 100}%',
+                    style: const TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
